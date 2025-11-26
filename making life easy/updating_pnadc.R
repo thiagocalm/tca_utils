89234f38cdb1d87333c25b3265a682ec9a90a076
thiagocalm <thiagocalm@outlook.com>
2025-11-26 21:25:55 +0100

    importing the whole series due to a change in the weights

--- making life easy/updating_pnadc.R
+++ making life easy/updating_pnadc.R
@@ -2,14 +2,15 @@
 #'@projeto TCA utils
 #'@responsavel Thiago Cordeiro-Almeida
 #'@script TCA
-#'@ultima-atualizacao 2025-11-16
+#'@ultima-atualizacao 2025-11-26
 #'@dados IBGE - PNADC
 #'@script Importacao e exportacao de bases da pnad para atualizacao
+#'@obs-ultima-atualizacao Atualizando toda a serie apos reponderacao da amostra
 #'---------------------------------------------------------
 
 # configuracoes gerais ----------------------------------------------------
 
-options(scipen = 99999, timeout = 1000)
+options(scipen = 9999, timeout = 1000)
 rm(list = ls())
 invisible(gc())
 
@@ -25,7 +26,7 @@ p_load(tidyverse, arrow, PNADcIBGE)
 ###
 
 # ano
-anos = c(2023,2024)
+anos = c(2012:2024)
 
 # visitas
 visitas = c(1,5)
@@ -39,31 +40,42 @@ for(i in seq_along(anos)){
   ano = anos[i]
   for(v in seq_along(visitas)){
     visita = visitas[v]
-    ###
-    # Importacao dos dados
-    ###
+    # condicionando para que nao faca a importacao em 2020 e 2021 da visita 1
+    if(ano %in% c(2020,2021) & visita == 1){
+      next
+    } else{
+      ###
+      # Importacao dos dados
+      ###
 
-    # importacao dos dados
+      # importacao dos dados
 
-    pnadc <- PNADcIBGE::get_pnadc(
-      year = ano,
-      interview = visita,
-      labels = FALSE,
-      design = FALSE
-    )
+      pnadc <- PNADcIBGE::get_pnadc(
+        year = ano,
+        interview = visita,
+        labels = FALSE,
+        design = FALSE
+      )
 
-    ###
-    # exportacao de dados
-    ###
+      # rapidas manipulacoes
 
-    write_parquet(pnadc, file.path(output_dir,glue::glue("pnadc_{ano}_visita_{visita}.parquet")))
+      pnadc <- pnadc |>
+        rename_with(tolower, .cols = everything()) |>
+        mutate(across(everything(),~ as.numeric(.x)))
 
-    ###
-    # next loop
-    ###
-    rm(pnadc)
-    invisible(gc())
-    print(paste0("Para bases de visitas, finalizamos: ",ano," - visita ",visita,"!"))
+      ###
+      # exportacao de dados
+      ###
+
+      write_parquet(pnadc, file.path(output_dir,glue::glue("pnadc_{ano}_visita_{visita}.parquet")))
+
+      ###
+      # next loop
+      ###
+      rm(pnadc)
+      invisible(gc())
+      print(paste0("Para bases de visitas, finalizamos: ",ano," - visita ",visita,"!"))
+    }
   }
 }
 
@@ -105,7 +117,7 @@ for(i in seq_along(anos)){
     # exportacao de dados
     ###
 
-    write_parquet(pnadc, file.path(output_dir,glue::glue("pnadc_{trimestre}{ano}_educ.parquet")))
+    write_parquet(pnadc, file.path(output_dir,glue::glue("pnadc_0{trimestre}{ano}_educ.parquet")))
 
     ###
     # next loop
