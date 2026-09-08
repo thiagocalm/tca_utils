2b4eb888ecc1e7ed2c181641c47c0b3dc21afdfc
Thiago Almeida <thiagocalm@outlook.com>
2026-09-08 11:37:32 +0200

    extending to incorporate packages not in cran

--- making life easy/importing_packages_and_datasets.R
+++ making life easy/importing_packages_and_datasets.R
@@ -7,9 +7,19 @@
 
 # check and load package
 
-check_and_load_package <- function(package_name) {
-  if (!requireNamespace(package_name, quietly = TRUE)) {
-    install.packages(package_name)
+check_and_load_package <- function(package_name, github = FALSE, package_rep = NULL) {
+  # package
+  require(pak)
+  # function
+  if(github == FALSE){
+    if (!requireNamespace(package_name, quietly = TRUE)) {
+      install.packages(package_name)
+    }
+    library(package_name, character.only = TRUE)
+  } else{
+    if (!requireNamespace(package_name, quietly = TRUE)) {
+      pak::pak(package_rep)
+    }
+    library(package_name, character.only = TRUE)
   }
-  library(package_name, character.only = TRUE)
 }
