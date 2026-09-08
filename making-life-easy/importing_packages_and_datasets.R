9e9d48cfc28c1b8c82e8e4b0ebea5f7737cc3890
Thiago Almeida <thiagocalm@outlook.com>
2026-09-08 11:47:20 +0200

    messages

--- making-life-easy/importing_packages_and_datasets.R
+++ making-life-easy/importing_packages_and_datasets.R
@@ -11,14 +11,21 @@ check_and_load_package <- function(package_name, github = FALSE, package_rep = N
   # package
   require(pak)
   # function
+  if(requireNamespace(package_name, quietly = TRUE)){
+    cat("package was already installed!")
+  }
   if(github == FALSE){
     if (!requireNamespace(package_name, quietly = TRUE)) {
+      cat("package was not installed. initiating install process...")
       install.packages(package_name)
+      cat("package installed and ready to use!")
     }
     library(package_name, character.only = TRUE)
   } else{
     if (!requireNamespace(package_name, quietly = TRUE)) {
+      cat("package was not installed. initiating install process...")
       pak::pak(package_rep)
+      cat("package installed and ready to use!")
     }
     library(package_name, character.only = TRUE)
   }
