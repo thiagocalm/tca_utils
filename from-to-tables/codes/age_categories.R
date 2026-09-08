47112efbd007e5b2275b4c099391e0ba112fb60e
Thiago Almeida <thiagocalm@outlook.com>
2026-09-08 12:07:09 +0200

    age categories transformation table

---
+++ from-to-tables/codes/age_categories.R
@@ -0,0 +1,10 @@
+
+# import package
+source("https://github.com/thiagocalm/tca_utils/raw/refs/heads/master/making-life-easy/importing_packages_and_datasets.R")
+check_and_load_package("wpp2024",github = TRUE, package_rep = "PPgp/wpp2024")
+
+# data
+data("age5categories")
+
+# export
+write.table(age5categories, file = file.path("from-to-tables","age5categories.txt"))
